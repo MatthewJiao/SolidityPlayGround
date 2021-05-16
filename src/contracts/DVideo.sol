378545614e7bc363fd1d249afb54773f1485509c
Matthew Jiao <jiaomatthew222@gmail.com>
2021-05-16 16:22:43 -0400

    passed tests

--- src/contracts/DVideo.sol
+++ src/contracts/DVideo.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.5.0;
 contract DVideo {
   uint public videoCount = 0;
   string public name = "DVideo";
+
   //Create id=>struct mapping
   mapping(uint => Video) public videos;
+
   //Create Struct
   struct Video {
     uint id;
@@ -13,13 +15,12 @@ contract DVideo {
     address author;
   }
 
-
   //Create Event
   event VideoUpload (
-    uint id;
-    string hash;
-    string title;
-    address author;
+    uint id,
+    string hash,
+    string title,
+    address author
   );
 
 
@@ -28,20 +29,23 @@ contract DVideo {
 
   function uploadVideo(string memory _videoHash, string memory _title) public {
     // Make sure the video hash exists
+    require(bytes(_videoHash).length > 0);
 
     // Make sure video title exists
+    require(bytes(_title).length > 0);
 
     // Make sure uploader address exists
+    require(msg.sender != address(0));
 
 
     // Increment video id
     videoCount ++;
-    // Add video to the contract
-    videos[videoCount] = Video(videoCount, _videoHash, _title, msg.sender) 
 
+    // Add video to the contract
+    videos[videoCount] = Video(videoCount, _videoHash, _title, msg.sender); 
 
     // Trigger an event
-    emit VideoUploaded(videoCount, _videoHash, _title, msg.sender)
+    emit VideoUpload(videoCount, _videoHash, _title, msg.sender);
 
   }
 }
